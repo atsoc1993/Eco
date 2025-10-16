7ee3d7d649c7ff119ed10e0184fd990de9e36788
atsoc1993 <atsoc1993@gmail.com>
2025-10-15 23:23:05 -0400

    Generated test account

---
+++ 0_generate_account.py
@@ -0,0 +1,10 @@
+from algosdk.account import generate_account
+from dotenv import load_dotenv, set_key
+
+load_dotenv('.env')
+sk, pk = generate_account()
+
+set_key('.env', 'sk', sk)
+set_key('.env', 'pk', pk)
+
+# fund via https://bank.testnet.algorand.network/
