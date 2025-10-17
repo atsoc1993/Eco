8bd392f03990f4965bb4e161a237f56650f3fda0
atsoc1993 <atsoc1993@gmail.com>
2025-10-16 23:31:39 -0400

    Added get typed eco app client function in constants

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 from algokit_utils import SigningAccount, AlgorandClient
-from contract_files.EcoClient import EcoFactory
+from contract_files.EcoClient import EcoFactory, EcoClient
 from dotenv import load_dotenv
 import os
 
@@ -20,5 +20,16 @@ eco_factory = EcoFactory(
     default_signer=test_account.signer,
 )
 
+eco_app_id = None
+
 if os.getenv('eco_app_id'):
-    eco_app_id = os.getenv('eco_app_id')
+    eco_app_id = int(os.getenv('eco_app_id'))
+
+def get_eco_typed_app_client(signing_account: SigningAccount) -> EcoClient:
+    if eco_app_id:
+        return algorand.client.get_typed_app_client_by_id(
+            typed_client=EcoClient,
+            app_id=eco_app_id,
+            default_sender=signing_account.address,
+            default_signer=signing_account.signer,
+        )
